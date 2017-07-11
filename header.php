1524c90da86a3ed0a3f89bb6d038bc0684e3cb92
Steve <stevecortis@live.com>
2017-07-11 08:57:09 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="apple-touch-icon" href="img/logo72.png">
+        <link rel="shortcut icon" href="img/favicon2.png">
+        <!-- Place favicon.ico in the root directory -->
+        <head>
+            <meta property="og:image" content="img/logo117.png" />
+            <link rel="image_src" href="img/logo117.png" / >
+            <meta name="description" content="" />
+            <title>Bidla - Change</title>
+        </head>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/style.css" />
+        <script src="js/vendor/modernizr.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,700,700i" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Bitter:400,400i,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Nobile" rel="stylesheet">
+
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here --> 
